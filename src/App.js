04421dcc0a17b5a54840ff94d657fc4ab3afed30
FrancescaMacioce <francesca.macioce@gmail.com>
2020-11-26 10:34:59 +0100

    fetch e context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+    
+    const [data,setData] = useState('');
+    const URL ='http://51.77.82.133:86/api/quotations/QUO_5fb3acb3a0f18';
+
+
+    useEffect( ()=> {
+        void (async () => {
+            try {
+                const res = await (await fetch(URL)).json()
+                console.log(res)
+                setData(res);
+            } catch (err) {
+                alert('Si è verificato un errore', err)
+            }
+        })()
+    }, []
+    )
+
+
     return (
         <div>
             <h1>Team 4</h1>
