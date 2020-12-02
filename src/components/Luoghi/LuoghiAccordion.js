7146910fc0967963b39da9b2542add111b7ce4b3
Giuseppe Ceniviva <cenipeppe@outlook.it>
2020-12-02 14:25:52 +0100

    allineamento pomeriggio

--- src/components/Luoghi/LuoghiAccordion.js
+++ src/components/Luoghi/LuoghiAccordion.js
@@ -7,17 +7,21 @@ function LuoghiAccordion({ city, date, content }) {
 
     return (
         <div className='d-flex justify-content-center mt-4'>
-            <div className="card bg-light mb-3 shadow">
-                <div className="card-header p-2 font-weight-bold text-uppercase">
-                    <strong className="align-middle" style={{ color: "#0063C9" }}>{city}</strong>
-                    <span className="align-middle" style={{ color: "#0d0d0d" }}>{date}</span>
-                    <i className={iconClass} onClick={() => setIsOpen(!isOpen)} /></div>
-                <div className="card-body">
-                    {(isOpen)
-                        ?
-                        <div />
-                        :
-                        <div />}
+            <div className="row">
+                <div className="col-lg-12" style={{ minWidth: '1300px' }}>
+                    <div className="arrotonda-card card bg-light mb-3 shadow">
+                        <div className="card-header p-2 font-weight-bold text-uppercase">
+                            <span className="align-middle" style={{ color: "#0063C9" }}>{city}</span>
+                            <span className="align-middle" style={{ color: "#0d0d0d" }}>{city}</span>
+                            <i className={iconClass} onClick={() => setIsOpen(!isOpen)} /></div>
+                        <div className="card-body" style={{ maxWidth: '1300px' }}>
+                            {(isOpen)
+                                ?
+                                <div />
+                                :
+                                <div />}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
