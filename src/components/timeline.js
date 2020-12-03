45faece12e539105f670fbd8cecb0642e19461b1
Alumina14 <fabiodigennaro14@gmail.com>
2020-12-03 16:05:37 +0100

    timeline

---
+++ src/components/timeline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function timeline() {
+    return (
+        <div className='tlContainer'>
+            <div className='Dot Big'>
+            </div>
+            <div className='Dot Medium'>                
+            </div>
+            <div className='Dot Small'>                
+            </div>
+        </div>
+    )
+}
+
+export default timeline
