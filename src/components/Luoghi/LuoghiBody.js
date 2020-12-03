70657b1e5f609aa128b1d22dec69eb8f40bb5456
Giuseppe Ceniviva <cenipeppe@outlook.it>
2020-12-03 15:27:58 +0100

    card luoghi completa

--- src/components/Luoghi/LuoghiBody.js
+++ src/components/Luoghi/LuoghiBody.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import LuoghiBodyAccomodation from './LuoghiBodyAccomodation'
 import LuoghiCar from './LuoghiCar'
 
 function LuoghiBody({rows, car}) {
@@ -10,9 +11,17 @@ function LuoghiBody({rows, car}) {
                         <p>{row.places[0].name}</p>
                         <p className="card-text p-4" style={{ fontSize: '17px', lineHeight: '20px' }}>{row.days[0].name}</p>
                         <p>{row.dayDate}</p>
-                        <img src={row.days[0].images[0].image} alt='villa' style={{width: 200, height: 200}}/>
+                        {/* <img src={row.days[0].images[0].image} alt='villa' style={{width: 200, height: 200}}/> */}
+                        {row.days[0].images.map(img=>{
+                            return(
+                                <img src={img.image} alt={img.image_name} key={img.id} style={{ width: 200, height: 200 }} />
+                            )
+                        })}
                         <p>{row.days[0].description}</p>
                         {(index===0 && car) && (<LuoghiCar />)}
+                        <p>Icona verde con spunta</p>
+                        <LuoghiBodyAccomodation accomodations={row.accomodations}/>
+                        <p>{row.included}</p>
                         <hr />
                     </div>
                 )
