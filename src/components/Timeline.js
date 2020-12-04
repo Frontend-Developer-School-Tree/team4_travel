fc7e7a036550c919487c6afa78ad09967f21a95c
Fabrizio Pulvirenti <fabrizio.pulvirenti.86@gmail.com>
2020-12-04 11:29:26 +0100

    fix

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 function Timeline() {
     return (
-        <div className='tlContainer'>
-            <div className='Dot Big'>
+            <div className='row'>
+                <div className='d-flex flex-column align-items-center col-1'>
+                    <div className='Dot Big'>
+                    </div>
+                    <div className='Dot Medium Medium2'>
+                    </div>
+                    <div className='Dot Medium'>
+                    </div>
+                    <div className='borderDot'>
+                    </div>
+                </div>
             </div>
-            <div className='Dot Medium'>                
-            </div>
-            <div className='Dot Small'>                
-            </div>
-        </div>
     )
 }
 
