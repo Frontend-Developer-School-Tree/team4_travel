3b189a67b0078b180513d412f3036c1a319cb499
Giuseppe Ceniviva <cenipeppe@outlook.it>
2020-12-10 10:50:07 +0100

    added Tariffe without css

--- src/components/StaticAccordion/AccordionContainer.js
+++ src/components/StaticAccordion/AccordionContainer.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
 import Accordion from './Accordion';
+import Tariffe from './Tariffe';
 
 
 function AccordionContainer() {
@@ -8,6 +9,7 @@ function AccordionContainer() {
     const { documentsRequested, documentsInsurance, documentsCancellation, documentsPayment } = contesto;
     return (
         <div>
+            <Tariffe title='TARIFFE' content={contesto} />
             <Accordion title={documentsRequested.name} content={documentsRequested.description}/>
             <Accordion title={documentsInsurance.name} content={documentsInsurance.description}/>
             <Accordion title={documentsCancellation.name} content={documentsCancellation.description}/>
